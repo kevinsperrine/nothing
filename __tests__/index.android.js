19a03994026a2311667626ee590b8fe9cb9328e8
Kevin <kperrine@gmail.com>
2017-06-20 15:10:49 -0500

    updates with tests and fastlane submissions

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,9 +1,18 @@
 import 'react-native';
 import React from 'react';
 import Index from '../index.android.js';
+import {
+  NativeModules,
+} from 'react-native';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+
+jest.mock('../AWSMobileHub', () => {
+  return {
+    initializeApplication: jest.fn()
+  };
+});
 
 it('renders correctly', () => {
   const tree = renderer.create(
