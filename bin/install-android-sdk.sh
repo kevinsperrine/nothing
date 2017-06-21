b4e74ab37e0125320ad89f928984f115de4a6da3
Kevin <kperrine@gmail.com>
2017-06-21 16:50:44 -0500

    ci

--- bin/install-android-sdk.sh
+++ bin/install-android-sdk.sh
@@ -7,8 +7,8 @@ brew cask install android-sdk
 # ensure PATH is set correctly
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 # install basic SDK tools
-if [ ! -e /usr/local/android-sdk-linux/platforms/android-23 ]; then sdkmanager "platforms;android-23"
-if [ ! -e /usr/local/android-sdk-linux/build-tools/23.0.1 ]; then sdkmanager "build-tools;23.0.1"
+sdkmanager "platforms;android-23"
+sdkmanager "build-tools;23.0.1"
 sdkmanager "tools"
 # ensure licenses are already accepted
 mkdir -p $ANDROID_HOME/licenses
