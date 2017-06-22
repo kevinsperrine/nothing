ea4563003cb5967411fe39ab40742b863dc39bc0
Kevin <kperrine@gmail.com>
2017-06-22 15:11:49 -0500

    increment locally or use CI build number if set.

--- bin/increment-android-version.sh
+++ bin/increment-android-version.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 GRADLE_BUILD="$1"
 
-awk '{sub(/versionCode [[:digit:]]+$/, "versionCode "$2+1)}1' $GRADLE_BUILD > gradle.tmp
+if [ -z ${CIRCLE_BUILD_NUM+x} ]; then
+    awk '{sub(/versionCode [[:digit:]]+$/, "versionCode "$2+1)}1' $GRADLE_BUILD > gradle.tmp
+else
+    awk -v version=$CIRCLE_BUILD_NUM '{sub(/versionCode [[:digit:]]+$/, "versionCode "version)}1' $GRADLE_BUILD > gradle.tmp
+fi
+
 mv -f gradle.tmp $GRADLE_BUILD
