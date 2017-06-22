579c99cede98d0a1ee68f58ab39ee4ccf72c3073
Kevin <kperrine@gmail.com>
2017-06-22 17:30:07 -0500

    increment correctly

--- bin/increment-android-version.sh
+++ bin/increment-android-version.sh
@@ -4,7 +4,8 @@ GRADLE_BUILD="$1"
 if [ -z ${CIRCLE_BUILD_NUM+x} ]; then
     awk '{sub(/versionCode [[:digit:]]+$/, "versionCode "$2+1)}1' $GRADLE_BUILD > gradle.tmp
 else
-    awk -v version=$CIRCLE_BUILD_NUM '{sub(/versionCode [[:digit:]]+$/, "versionCode "version)}1' $GRADLE_BUILD > gradle.tmp
+    VERSION=$(( $CIRCLE_BUILD_NUM + 52 ));
+    awk -v version=$VERSION '{sub(/versionCode [[:digit:]]+$/, "versionCode "version)}1' $GRADLE_BUILD > gradle.tmp
 fi
 
 mv -f gradle.tmp $GRADLE_BUILD
